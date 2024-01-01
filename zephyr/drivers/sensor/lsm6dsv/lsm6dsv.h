ec5700c2337fe572938f7fd2c54743f4c761fb80
gorbit99 <gorbitgames@gmail.com>
2024-01-01 01:36:20 +0100

    Base files

---
+++ zephyr/drivers/sensor/lsm6dsv/lsm6dsv.h
@@ -0,0 +1,12 @@
+#ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_LSM6DSV_H_
+#define ZEPHYR_INCLUDE_DRIVERS_SENSOR_LSM6DSV_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
